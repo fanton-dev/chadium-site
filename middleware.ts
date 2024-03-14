b9b3989a931973c4845ca8b7ee1925771ff75739
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-03-14 02:11:33 +0200

    chore: client component translations
    Added configuration for client side components.

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,10 @@
 import createMiddleware from 'next-intl/middleware';
 
+export const locales = ['en'];
+
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en'],
+  locales,
 
   // Used when no locale matches
   defaultLocale: 'en',
