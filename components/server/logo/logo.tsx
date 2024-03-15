9032c27eeb4a54ebb41ec5e415f64c84329f8091
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-03-16 00:56:50 +0200

    refactor: update logo in server component
    The logo in the server component has been updated. The commented code has been replaced with an Image component sourced from a new logo file.
    Additionally, the size of the logo and accompanying text has been slightly increased.

--- components/server/logo/logo.tsx
+++ components/server/logo/logo.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/lib/utils';
 import { HTMLAttributes } from 'react';
+import Image from 'next/image';
 
 export function Logo(props: HTMLAttributes<HTMLDivElement>) {
   // eslint-disable-next-line react/prop-types
@@ -9,13 +10,13 @@ export function Logo(props: HTMLAttributes<HTMLDivElement>) {
       className={cn('flex w-fit items-center gap-1.5', className)}
       {...otherProps}
     >
-      {/*<Image*/}
-      {/*  src="/assets/icons/sugaming.png"*/}
-      {/*  alt="CHADIUM Logo"*/}
-      {/*  width={32}*/}
-      {/*  height={32}*/}
-      {/*/>*/}
-      <span className="text-xl font-bold">CHADIUM</span>
+      <Image
+        src="/assets/images/logo/logo.png"
+        alt="CHADIUM Logo"
+        width={42}
+        height={42}
+      />
+      <span className="text-2xl font-bold">CHADIUM</span>
     </div>
   );
 }
