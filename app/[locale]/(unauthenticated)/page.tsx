ad4953e44605cbb1fe052b99fa84d0408a6a9dd3
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-03-16 06:38:21 +0200

    feat: update home page layout and image assets
    Revised the layout of the home page to incorporate a banner and logo. The main div's classes have been adjusted for proper alignment and dimension, with an added overlay div to properly place the logo. Furthermore, image attributes have been updated to reflect new assets.

--- app/[locale]/(unauthenticated)/page.tsx
+++ app/[locale]/(unauthenticated)/page.tsx
@@ -4,13 +4,24 @@ import Image from 'next/image';
 export default async function Home() {
   const t = await getTranslations('pages.home');
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="relative flex h-screen flex-col items-center justify-between -mt-14">
       <Image
-        src="/assets/images/logo/logo.png"
-        alt="CHADIUM Logo"
-        width={800}
-        height={800}
+        src="/assets/images/logo/chadium_poster.png"
+        alt="CHADIUM Banner"
+        width={1920}
+        height={468}
+        className="absolute inset-0 object-cover w-full h-full"
       />
-    </main>
+
+      <div className="absolute inset-0  flex w-full h-full justify-center items-center">
+        <Image
+          src="/assets/images/logo/logo.png"
+          alt="CHADIUM Logo"
+          width={800}
+          height={800}
+          className="h-2/3 w-auto"
+        />
+      </div>
+    </div>
   );
 }
